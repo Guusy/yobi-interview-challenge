41d1081b6b6cb4c71aae9ff2a33730b70bce11fb
Guusy <guusygonzalo@gmail.com>
2019-07-07 20:27:22 -0300

    add remove product action

---
+++ src/actions/removeProduct/removeProductAction.js
@@ -0,0 +1,10 @@
+export default (lotId) => ({
+    type: "REMOVE_PRODUCT",
+    reducer: (state) => {
+        const newProducts = state.products.filter(product => product.lotId !== lotId)
+        return {
+            ...state,
+            products: newProducts
+        }
+    }
+})
